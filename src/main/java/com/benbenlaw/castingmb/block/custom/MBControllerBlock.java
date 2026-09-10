0c52c01d5aaf53b7de03a551a438dcf89faf3381
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-09-10 08:33:16 +0100

    1.9.4

--- src/main/java/com/benbenlaw/castingmb/block/custom/MBControllerBlock.java
+++ src/main/java/com/benbenlaw/castingmb/block/custom/MBControllerBlock.java
@@ -9,9 +9,11 @@ import com.benbenlaw.castingmb.block.entity.MBControllerBlockEntity;
 import com.benbenlaw.castingmb.block.entity.MBSolidifierBlockEntity;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -35,11 +37,11 @@ public class MBControllerBlock extends CastingBlock {
     }
 
     @Override
-    protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull BlockHitResult hitResult) {
+    protected InteractionResult useItemOn(ItemStack itemStack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
         if (!level.isClientSide()) {
             BlockEntity entity = level.getBlockEntity(pos);
             if (entity instanceof MBControllerBlockEntity entity1) {
-                if (entity1.onPlayerUse(player, player.getUsedItemHand())) {
+                if (entity1.onPlayerUse(player, hand)) {
                     return InteractionResult.SUCCESS;
                 }
                 player.openMenu(new SimpleMenuProvider(entity1, entity1.getDisplayName()), pos);
--- src/main/java/com/benbenlaw/castingmb/block/custom/MBSolidifierBlock.java
+++ src/main/java/com/benbenlaw/castingmb/block/custom/MBSolidifierBlock.java
@@ -8,9 +8,11 @@ import com.benbenlaw.castingmb.block.CastingMBBlockEntities;
 import com.benbenlaw.castingmb.block.entity.MBSolidifierBlockEntity;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -34,11 +36,11 @@ public class MBSolidifierBlock extends CastingBlock {
     }
 
     @Override
-    protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull BlockHitResult hitResult) {
+    protected InteractionResult useItemOn(ItemStack itemStack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
         if (!level.isClientSide()) {
             BlockEntity entity = level.getBlockEntity(pos);
             if (entity instanceof MBSolidifierBlockEntity entity1) {
-                if (entity1.onPlayerUse(player, player.getUsedItemHand())) {
+                if (entity1.onPlayerUse(player, hand)) {
                     return InteractionResult.SUCCESS;
                 }
                 player.openMenu(new SimpleMenuProvider(entity1, entity1.getDisplayName()), pos);
